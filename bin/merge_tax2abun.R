c2ca3977f95465d5576ff5b8401ebdfc4e387502
twilightBI <twilight_yang@163.com>
2023-09-11 17:52:03 +0800

    date 23-9-11 contig分类信息合并脚本bug修复，过滤掉均未注释到的contig

--- bin/merge_tax2abun.R
+++ bin/merge_tax2abun.R
@@ -21,7 +21,14 @@ names(dat)[1] <- "ID"
 names(abun)[1] <- "ID"
 
 dat$taxonomy <- gsub(" ","_",dat$taxonomy)
+
+empty_taxonomy_rows <- dat$taxonomy == ""
 #dat$taxonomy <- gsub("p__;|c__;|o__;|f__;|g__;","",dat$taxonomy)
+
+# 使用这些行来过滤 dat_1 数据框
+dat_empty_taxonomy <- dat[empty_taxonomy_rows, ]
+dat <- dat[!(dat$ID %in% dat_empty_taxonomy$ID), ]
+
 
 #Remove rows with abundance values that are all 0
 if(ncol(abun)==2){
@@ -31,17 +38,27 @@ if(ncol(abun)==2){
 	abun <- abun[rowSums(abun[,2:ncol(abun)])>0,]
 }
 
+# 获取abun和dat的ID列的交集
+common_ids <- intersect(abun$ID, dat$ID)
+# 筛选出abun和dat中与交集中的ID匹配的行
+abun_filtered <- abun[abun$ID %in% common_ids, ]
+dat_filtered <- dat[dat$ID %in% common_ids, ]
+
+# 合并筛选后的数据
+dat_1 <- merge(abun_filtered, dat_filtered, by="ID", sort=FALSE)
+
 #Merge species annotation table and contigs abundance table
-dat_1 <- merge(abun,dat,id="ID",sort = FALSE) 
+#dat_1 <- merge(abun,dat,id="ID",sort = FALSE) 
 
 dat_2 <- data.frame(dat_1[,c("ID","taxonomy")],
                     dat_1[,names(abun)[2:ncol(abun)]],check.names = FALSE)
 dat_2 <- subset(dat_2,select = -c(ID))
 
+
 #Sum the abundance values of species annotations
 dat_sum <- aggregate(dat_2[,2:ncol(dat_2)],by=list(dat_2$taxonomy),sum)
 names(dat_sum)[1] <- "Taxonomy"
-dat_sum <- dat_sum[order(dat_sum$Taxonomy,decreasing = FALSE),]
 
+dat_sum <- dat_sum[order(dat_sum$Taxonomy,decreasing = FALSE),]
 write.table(dat_1,paste0(outFile,".contig.with.tax.txt"),row.names = F,sep = '\t',quote = F)
 write.table(dat_sum,paste0(outFile,".taxonomy.txt"),row.names = F,sep = '\t',quote = F)
